2154e28170e554dff551fad0fc9220adec5909b0
Bastien Dechamps <bastien.dechamps2@gmail.com>
2019-12-31 15:49:59 +0100

    add: ising finalized + plot of Z

--- HW2/ising.py
+++ HW2/ising.py
@@ -1,36 +1,61 @@
 import itertools
-
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sum_product import SumProductUndirectedChain
 
-w = 10
-h = 100
 
-alpha = 0
-beta = 0.5
+class IsingGrid:
+    def __init__(self, w, h, alpha, beta):
+        self.w = w
+        self.h = h
+        self.beta = beta
+        self.alpha = alpha
 
-x = np.array(list(itertools.product(*[[0, 1]] * w)))
-assert x.shape == (2 ** w, w)
+    def junction_tree(self):
+        x = np.array(list(itertools.product(*[[0, 1]] * self.w)))
 
-phi_simple = np.exp(alpha * x.sum(1)) * np.exp(beta * (x[:, :-1] == x[:, 1:]).sum(1))
-assert phi_simple.shape == (1024,)
+        phi_simple_log = self.alpha * x.sum(1) + self.beta * (
+            x[:, :-1] == x[:, 1:]
+        ).sum(1)
 
-# SLOW
-phi_double = np.empty((2 ** w), (2**w))
-for i in range(2 ** w):
-    for j in range(2 ** w):
-        phi_double[] # TODO
+        phi_double_log = np.array(
+            [
+                [self.beta * (x[i] == x[j]).sum() for j in range(2 ** w)]
+                for i in range(2 ** w)
+            ]
+        )
+
+        phi_simple_log_chain = np.array([phi_simple_log for _ in range(self.h)])
+        phi_double_log_chain = np.array([phi_double_log for _ in range(self.h)])
+
+        chain = SumProductUndirectedChain(phi_simple_log_chain, phi_double_log_chain)
+
+        return chain
 
 
-phi_double = np.exp(beta * (x[:-1, :] == x[1:, :]).sum(0)) # TODO
-assert phi_double.shape == (1024, 1024)
+if __name__ == "__main__":
+    w = 10
+    h = 100
 
+    alpha = 0.0
 
-# chain = SumProductUndirectedChain(phi_double, phi_double)
+    betas = np.linspace(0.0, 2.0, 50)
+    log_zs = []
 
-# assert len(chain) == 100
+    for i, beta in enumerate(betas):
+        model = IsingGrid(w, h, alpha, beta)
+        chain = model.junction_tree()
+        chain.propagate()
+        chain.marginalize(0)
 
-# chain.propagate()
+        log_zs.append(chain.logZ)
 
-# Z = chain.Z
+    # Plot it
+    plt.figure(figsize=(4, 4))
+    plt.plot(betas, log_zs, lw=2.0)
+    plt.title("Partition function $Z(0, \\beta)$")
+    plt.xlabel("$\\beta$")
+    plt.ylabel("$\log Z(0, \\beta)$")
+    plt.grid("on")
+    plt.show()
