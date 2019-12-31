65ebb2dce08747b2144a03dec28f05ee6c5e5a5a
Bastien Dechamps <bastien.dechamps2@gmail.com>
2019-12-31 13:05:05 +0100

    fix: sum product algorithm + more relevant test

--- HW2/sum_product.py
+++ HW2/sum_product.py
@@ -1,48 +1,51 @@
-import numpy as np 
+import numpy as np
 from scipy.special import logsumexp
+import scipy.stats as stats
+
 
 class SumProductUndirectedChain:
-    
-    def __init__(self, psis_single, psis_double):
-        self.psis_single = np.log(psis_single)
-        self.psis_double = np.log(psis_double)
+    def __init__(self, psis_single_log, psis_double_log):
+        """Initialize the undirected chain.
+        Args:
+            - psis_single_log: list of N np.array (node potential)
+            - psis_double_log: list of N-1 np.array (edge potential)
+        """
+        self.psis_single_log = psis_single_log
+        self.psis_double_log = psis_double_log
         self.mu_asc = []
         self.mu_desc = []
         self._Z = None
         self.propagated = False
 
     def __len__(self):
-        return len(self.psis_single)
+        return len(self.psis_single_log)
 
     def propagate(self):
-        # Ascending
-        self.mu_asc.append(np.zeros_like(self.psis_single[0]))
-        for idx in range(len(self)):
-            # Vector of shape (nb of states of node idx,)
-            msg_single = self.mu_asc[idx] + self.psis_single[idx]
-            # Matrix of shape (nb of states of node idx+1, nb of states of node idx) 
-            msg_double = self.psis_double[idx].T
-            # Append final message of shape (nb of states of node idx+1,)
-            self.mu_asc.append(logsumexp(msg_double + msg_single, axis=1))
+        # Ascending messages
+        mu_asc = 0
+        self.mu_asc.append(np.zeros_like(self.psis_single_log[0]))
+        for i in range(len(self) - 1):
+            msg_single = mu_asc + self.psis_single_log[i]
+            msg_double = self.psis_double_log[i].T
+            mu_asc = logsumexp(msg_single + msg_double, axis=1)
+            self.mu_asc.append(mu_asc)
 
-        # Descending
-        self.mu_desc.append(np.zeros_like(self.psis_single[-1]))
-        for idx in reversed(range(len(self))):
-            # Vector of shape (nb of states of node idx,)
-            msg_single = self.mu_desc[len(self) - 1 - idx] + self.psis_single[idx]
-            # Matrix of shape (nb of states of node idx+1, nb of states of node idx) 
-            msg_double = self.psis_double[idx].T
-            # Append final message of shape (nb of states of node idx+1,)
-            self.mu_desc.append(logsumexp(msg_double + msg_single, axis=1))
+        # Descending messages
+        mu_desc = 0
+        self.mu_desc.append(np.zeros_like(self.psis_single_log[-1]))
+        for i in range(len(self) - 1, 0, -1):
+            msg_single = mu_desc + self.psis_single_log[i]
+            msg_double = self.psis_double_log[i - 1]
+            mu_desc = logsumexp(msg_single + msg_double, axis=1)
+            self.mu_desc.append(mu_desc)
+        self.mu_desc.reverse()
 
-        self.mu_desc = list(reversed(self.mu_desc))
         self.propagated = True
-       
 
     def marginalize(self, idx):
         assert self.propagated, "Call propagate before marginalizing"
         # Compute un-normalized log probs
-        log_probs = self.psis_single[idx] + self.mu_asc[idx] + self.mu_desc[idx]
+        log_probs = self.psis_single_log[idx] + self.mu_asc[idx] + self.mu_desc[idx]
 
         # Get back from log scale
         probs = np.exp(log_probs)
@@ -50,7 +53,7 @@ class SumProductUndirectedChain:
         # Normalize
         self._Z = np.sum(probs)
         probs /= self._Z
-        
+
         return probs
 
     @property
@@ -63,10 +66,31 @@ class SumProductUndirectedChain:
 
 if __name__ == "__main__":
     # Test the sum product algorithm on independant Bernouilli distributions
+    # Dimensions of each distribution
+    n1, n2, n3 = 2, 5, 20
 
-    psis_single = [np.array([0.3, 0.7]), np.array([0.5, 0.5]), np.array([0.25, 0.75])]
-    psis_double = [np.ones((2, 2)) for i in range(3)]
+    # Bernouilli (unormalized)
+    psi1 = np.array([4.0, 1.0])
 
-    A = SumProductUndirectedChain(psis_single, psis_double)
-    A.propagate()
-    A.marginalize(0)
+    # Binomial (unormalized)
+    psi2 = 5.0 * stats.binom.pmf(np.arange(n2), 5, 0.4)
+
+    # Poisson (unormalized)
+    psi3 = 4.0 * stats.poisson.pmf(np.arange(n3), 10.0)
+
+    psis_single = [psi1, psi2, psi3]
+
+    # No iteraction
+    psis_double = [
+        np.ones((n1, n2)),
+        np.ones((n2, n3)),
+    ]
+
+    psis_single_log = [np.log(p) for p in psis_single]
+    psis_double_log = [np.log(p) for p in psis_double]
+
+    chain = SumProductUndirectedChain(psis_single_log, psis_double_log)
+    chain.propagate()
+    chain.marginalize(0)
+
+    print("Partition function: Z =", chain.Z)
